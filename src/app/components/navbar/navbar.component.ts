021aa5bd7d44609f697bc9b80c3795d1233c914f
JorgeRiveraMancilla <jiriveramancilla@gmail.com>
2024-11-28 23:32:58 -0300

    feat: sections depending on whether the user is authenticated

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,21 +1,60 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterModule } from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
 import { ButtonModule } from 'primeng/button';
+import { AuthService } from '../../services/auth.service';
+import { MenuItem } from 'primeng/api';
+import { MenuModule } from 'primeng/menu';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [CommonModule, RouterModule, ButtonModule],
+  imports: [CommonModule, RouterModule, ButtonModule, MenuModule],
   templateUrl: './navbar.component.html',
   styles: ``,
 })
 export class NavbarComponent {
-  navigateToLogin() {
-    //  TODO: Implementar navegación al login
+  private readonly router = inject(Router);
+  private readonly authService = inject(AuthService);
+
+  userMenuItems: MenuItem[] = [
+    {
+      label: 'Editar perfil',
+      icon: 'pi pi-user-edit',
+      command: () => this.navigateToProfile(),
+    },
+    {
+      separator: true,
+    },
+    {
+      label: 'Cerrar sesión',
+      icon: 'pi pi-sign-out',
+      command: () => this.logout(),
+    },
+  ];
+
+  get isAuthenticated(): boolean {
+    return this.authService.isAuthenticated();
   }
 
-  navigateToRegister() {
-    // TODO: Implementar navegación al registro
+  get currentUser(): string | undefined {
+    return this.authService.getCurrentAuth()?.userName;
+  }
+
+  navigateToLogin(): void {
+    this.router.navigate(['/login']);
+  }
+
+  navigateToRegister(): void {
+    this.router.navigate(['/register']);
+  }
+
+  navigateToProfile(): void {
+    this.router.navigate(['/profile']);
+  }
+
+  logout(): void {
+    this.authService.logout();
+    this.router.navigate(['/']);
   }
 }
