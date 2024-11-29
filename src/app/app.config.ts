db92ad5e83bc56f17049320f5ec1dd0c31be2e08
JorgeRiveraMancilla <jiriveramancilla@gmail.com>
2024-11-28 23:43:59 -0300

    feat: define message service provider from primeng

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,9 +5,11 @@ import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideHttpClient, withFetch } from '@angular/common/http';
+import { MessageService } from 'primeng/api';
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    MessageService,
     provideHttpClient(withFetch()),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
