f2aa1c17b2c5de691fb5d179689f52c97356d56f
JorgeRiveraMancilla <jiriveramancilla@gmail.com>
2024-11-29 01:30:16 -0300

    feat: implement user service

---
+++ src/app/services/user.service.ts
@@ -0,0 +1,124 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Injectable, inject, signal } from '@angular/core';
+import { of, map, Observable } from 'rxjs';
+import { environment } from '../../environments/environment';
+import { PaginatedResult } from '../interfaces/paginated-result';
+import { User } from '../interfaces/user';
+import { UserParams } from '../interfaces/user-params';
+import { AuthService } from './auth.service';
+import { UserUpdate } from '../interfaces/user-update';
+import { setPaginatedResponse } from './paginationHelper';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class UserService {
+  private readonly http = inject(HttpClient);
+  private readonly authService = inject(AuthService);
+  private readonly baseUrl = environment.apiUrl;
+
+  paginatedResult = signal<PaginatedResult<User[]> | null>(null);
+  userCache = new Map();
+  auth = this.authService.getCurrentAuth();
+  userParams: UserParams = new UserParams(this.auth);
+
+  getUsers(userParams: UserParams) {
+    const response = this.userCache.get(Object.values(userParams).join('-'));
+
+    if (response) return of(response, this.paginatedResult);
+
+    const params = new HttpParams()
+      .append('minAge', userParams.minAge)
+      .append('maxAge', userParams.maxAge)
+      .append('gender', userParams.gender)
+      .append('orderBy', userParams.orderBy);
+
+    return this.http
+      .get<User[]>(`${this.baseUrl}/user`, { observe: 'response', params })
+      .subscribe({
+        next: (response) => {
+          setPaginatedResponse(response, this.paginatedResult);
+          this.userCache.set(
+            Object.values(this.userParams).join('-'),
+            response
+          );
+        },
+      });
+  }
+
+  getUser(id: number): Observable<User> {
+    const user = [...this.userCache.values()]
+      .reduce((arr, elem) => arr.concat(elem.result), [])
+      .find((user: User) => user.id === id);
+
+    if (user) return of(user);
+
+    return this.http.get<User>(`${this.baseUrl}/user/${id}`);
+  }
+
+  updateUser(userId: number, updateUser: UserUpdate): Observable<void> {
+    return this.http
+      .put<void>(`${this.baseUrl}/user/${userId}`, updateUser)
+      .pipe(
+        map(() => {
+          if (this.auth && this.auth.userId === userId) {
+            this.auth = { ...this.auth, ...updateUser };
+            this.authService.setCurrentAuth(this.auth);
+          }
+
+          const updatedResult = this.paginatedResult()?.result.map((user) =>
+            user.id === userId ? { ...user, ...updateUser } : user
+          );
+
+          if (updatedResult) {
+            this.paginatedResult.set({
+              ...this.paginatedResult()!,
+              result: updatedResult,
+            });
+          }
+
+          [...this.userCache.keys()].forEach((key) => {
+            const cache = this.userCache.get(key);
+            if (cache) {
+              const userToUpdate = cache.result.find(
+                (user: User) => user.id === userId
+              );
+              if (userToUpdate) {
+                Object.assign(userToUpdate, updateUser);
+              }
+            }
+          });
+        })
+      );
+  }
+
+  setMainPhoto(photoId: number): Observable<void> {
+    return this.http.put<void>(
+      `${this.baseUrl}/user/set-main-photo/${photoId}`,
+      {}
+    );
+  }
+
+  deletePhoto(photoId: number): Observable<void> {
+    return this.http.delete<void>(
+      `${this.baseUrl}/user/delete-photo/${photoId}`
+    );
+  }
+
+  getUserParams(): UserParams | undefined {
+    return this.userParams;
+  }
+
+  setUserParams(userParams: UserParams): void {
+    this.userParams = userParams;
+  }
+
+  resetUserParams() {
+    if (this.auth) {
+      const resetParams = new UserParams(this.auth);
+      this.userParams = resetParams;
+      return resetParams;
+    }
+    return undefined;
+  }
+}
