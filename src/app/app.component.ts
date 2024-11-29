0a646a61d162a857b72224f9c5449cf1804ad337
JorgeRiveraMancilla <jiriveramancilla@gmail.com>
2024-11-29 00:39:59 -0300

    feat: add toast module

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,18 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { ToastModule } from 'primeng/toast';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, NavbarComponent, FooterComponent],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    NavbarComponent,
+    FooterComponent,
+    ToastModule,
+  ],
   templateUrl: './app.component.html',
   styles: ``,
 })
