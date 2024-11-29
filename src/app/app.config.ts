722ce6a0dd8d4dc88dc21d778550063da2fed535
JorgeRiveraMancilla <jiriveramancilla@gmail.com>
2024-11-29 00:38:14 -0300

    feat: add jwt interceptor and toast module in providers

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,21 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import {
+  ApplicationConfig,
+  importProvidersFrom,
+  provideZoneChangeDetection,
+} from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideAnimations } from '@angular/platform-browser/animations';
-import { provideHttpClient, withFetch } from '@angular/common/http';
+import {
+  provideHttpClient,
+  withFetch,
+  withInterceptors,
+} from '@angular/common/http';
 import { MessageService } from 'primeng/api';
+import { jwtInterceptor } from './interceptors/jwt.interceptor';
+import { ToastModule } from 'primeng/toast';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -15,5 +25,7 @@ export const appConfig: ApplicationConfig = {
     provideRouter(routes),
     provideClientHydration(),
     provideAnimations(),
+    provideHttpClient(withInterceptors([jwtInterceptor])),
+    importProvidersFrom(ToastModule),
   ],
 };
