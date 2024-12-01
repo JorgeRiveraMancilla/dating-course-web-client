e14c33293ef9158a12ed87475cc588949cf9157d
JorgeRiveraMancilla <jiriveramancilla@gmail.com>
2024-12-01 15:25:07 -0300

    feat: define routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { LoginPageComponent } from './pages/login-page/login-page.component';
+import { RegisterPageComponent } from './pages/register-page/register-page.component';
+import { authGuard } from './guards/auth.guard';
+import { UserPageComponent } from './pages/user-page/user-page.component';
+import { NotFoundPageComponent } from './pages/not-found-page/not-found-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomePageComponent },
+  { path: 'login', component: LoginPageComponent },
+  { path: 'register', component: RegisterPageComponent },
+  {
+    path: '',
+    canActivate: [authGuard],
+    children: [{ path: 'users', component: UserPageComponent }],
+  },
+  { path: 'not-found', component: NotFoundPageComponent },
+  { path: '**', redirectTo: 'not-found', pathMatch: 'full' },
+];
