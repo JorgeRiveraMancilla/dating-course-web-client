a25ff0cd64a46f130d7776c0e887df6e067a5233
JorgeRiveraMancilla <jiriveramancilla@gmail.com>
2024-12-01 15:22:52 -0300

    feat: implement user card component

---
+++ src/app/components/user-card/user-card.component.ts
@@ -0,0 +1,26 @@
+import { Component, Input } from '@angular/core';
+import { ButtonModule } from 'primeng/button';
+import { CardModule } from 'primeng/card';
+import { User } from '../../interfaces/user';
+
+@Component({
+  selector: 'app-user-card',
+  standalone: true,
+  imports: [CardModule, ButtonModule],
+  templateUrl: './user-card.component.html',
+  styles: ``,
+})
+export class UserCardComponent {
+  @Input() user!: User;
+
+  defaultImageUrl = '/assets/user.png';
+  hover = false;
+
+  onProfileView(): void {}
+
+  onLike(): void {}
+
+  onChat(): void {}
+
+  onLikeProfile(): void {}
+}
