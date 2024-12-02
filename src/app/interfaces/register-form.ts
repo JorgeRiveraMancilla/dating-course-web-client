e5609f6870b505bcc63b24c0afc3e7e99f1501a2
JorgeRiveraMancilla <jiriveramancilla@gmail.com>
2024-12-01 21:28:02 -0300

    feat: add register form interface

---
+++ src/app/interfaces/register-form.ts
@@ -0,0 +1,11 @@
+export interface RegisterForm {
+  userName: string;
+  email: string;
+  knownAs: string;
+  gender: string;
+  birthDate: Date;
+  city: string;
+  country: string;
+  password: string;
+  confirmPassword: string;
+}
