1c3b53cfe41c79fb5d305db9e6f08d84edfe7673
JorgeRiveraMancilla <jiriveramancilla@gmail.com>
2024-12-03 01:26:17 -0300

    feat: define navigations

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,12 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { Component, inject, computed } from '@angular/core';
-import { RouterModule } from '@angular/router';
-import { Router } from '@angular/router';
+import { RouterModule, Router } from '@angular/router';
 import { MenuItem } from 'primeng/api';
 import { ButtonModule } from 'primeng/button';
 import { AuthService } from '../../services/auth.service';
 import { MenuModule } from 'primeng/menu';
 import { ProgressSpinnerModule } from 'primeng/progressspinner';
+import { MenubarModule } from 'primeng/menubar';
 
 @Component({
   selector: 'app-navbar',
@@ -17,20 +17,19 @@ import { ProgressSpinnerModule } from 'primeng/progressspinner';
     ButtonModule,
     MenuModule,
     ProgressSpinnerModule,
+    MenubarModule,
   ],
   templateUrl: './navbar.component.html',
 })
 export class NavbarComponent {
-  private readonly router = inject(Router);
   private readonly authService = inject(AuthService);
+  protected readonly router = inject(Router);
 
   readonly isAuthenticated = computed(() => this.authService.isAuthenticated());
   readonly isAuthInitialized = computed(() =>
     this.authService.isAuthInitialized()
   );
-  readonly currentUser = computed(
-    () => this.authService.getCurrentAuth()?.userName
-  );
+  readonly currentUser = computed(() => this.authService.getCurrentAuth());
 
   protected readonly userMenu: {
     items: MenuItem[];
@@ -39,23 +38,40 @@ export class NavbarComponent {
       {
         label: 'Editar perfil',
         icon: 'pi pi-user-edit',
-        command: () => this.navigateTo('/profile'),
+        command: () => this.onEditProfileClick(),
       },
       { separator: true },
       {
         label: 'Cerrar sesión',
         icon: 'pi pi-sign-out',
-        command: () => this.handleLogout(),
+        command: () => this.onLogoutClick(),
       },
     ],
   };
 
-  protected navigateTo(route: string): void {
-    this.router.navigate([route]);
+  protected onLoginClick(): void {
+    this.router.navigate(['login']);
   }
 
-  private handleLogout(): void {
+  protected onRegisterClick(): void {
+    this.router.navigate(['register']);
+  }
+
+  protected onHomeClick(): void {
+    this.router.navigate(['']);
+  }
+
+  protected onUsersClick(): void {
+    console.log('Navegando a users...');
+    this.router.navigate(['users']);
+  }
+
+  private onEditProfileClick(): void {
+    this.router.navigate(['users', this.currentUser()?.userId, 'edit']);
+  }
+
+  private onLogoutClick(): void {
     this.authService.logout();
-    this.navigateTo('/');
+    this.router.navigate(['login']);
   }
 }
