de4ddabef27cede3910a9bd65c06b5bb3757ee90
JorgeRiveraMancilla <jiriveramancilla@gmail.com>
2024-12-16 03:52:15 -0300

    feat: add application startup authentication

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ToastModule } from 'primeng/toast';
 import { FooterComponent } from './components/footer/footer.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { ConfirmDialogModule } from 'primeng/confirmdialog';
+import { ProgressSpinnerModule } from 'primeng/progressspinner';
+import { AuthService } from './services/auth.service';
 
 @Component({
   selector: 'app-root',
@@ -16,7 +18,20 @@ import { ConfirmDialogModule } from 'primeng/confirmdialog';
     FooterComponent,
     ToastModule,
     ConfirmDialogModule,
+    ProgressSpinnerModule,
   ],
   templateUrl: './app.component.html',
 })
-export class AppComponent {}
+export class AppComponent implements OnInit {
+  private readonly authService = inject(AuthService);
+  protected loading = true;
+
+  async ngOnInit() {
+    try {
+      await this.authService.initializeAuth();
+      this.loading = false;
+    } catch (error) {
+      this.loading = false;
+    }
+  }
+}
