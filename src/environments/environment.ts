6925f7c0c5c8043244e961926004427b9692f3c4
JorgeRiveraMancilla <jiriveramancilla@gmail.com>
2025-01-02 13:13:53 -0300

    chore: update env variables

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  apiUrl: import.meta.env['VITE_API_URL'] || 'api',
-  hubUrl: import.meta.env['VITE_HUB_URL'] || 'hub',
+  apiUrl: process.env['NG_API_URL'] || 'api',
+  hubUrl: process.env['NG_HUB_URL'] || 'hub',
   authStorageKey: 'auth',
   debounceMilliseconds: 3000,
   defaultUserImageUrl: '/assets/user.png',
