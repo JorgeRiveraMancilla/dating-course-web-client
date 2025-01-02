4a4d2460ab8a64db7c0c752234d56e23390e8717
JorgeRiveraMancilla <jiriveramancilla@gmail.com>
2025-01-02 15:45:25 -0300

    fix: add url protocol

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  apiUrl: 'dating-course-evcbghbugadafues.brazilsouth-01.azurewebsites.net/api',
-  hubUrl: 'dating-course-evcbghbugadafues.brazilsouth-01.azurewebsites.net/hub',
+  apiUrl: 'https://dating-course-evcbghbugadafues.brazilsouth-01.azurewebsites.net/api',
+  hubUrl: 'https://dating-course-evcbghbugadafues.brazilsouth-01.azurewebsites.net/hub',
   authStorageKey: 'auth',
   debounceMilliseconds: 0,
   defaultUserImageUrl: '/assets/user.png',
