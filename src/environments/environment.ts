b13b71d8ebb20068ab3e5b22963e6e268574a044
JorgeRiveraMancilla <jiriveramancilla@gmail.com>
2025-01-02 15:10:23 -0300

    fix: update env variables

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  apiUrl: process.env['VITE_API_URL'] || 'api',
-  hubUrl: process.env['VITE_HUB_URL'] || 'hub',
+  apiUrl: process.env['API_URL'],
+  hubUrl: process.env['HUB_URL'],
   authStorageKey: 'auth',
   debounceMilliseconds: 0,
   defaultUserImageUrl: '/assets/user.png',
